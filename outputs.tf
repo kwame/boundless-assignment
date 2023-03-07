a4bb9f0cc05995975d58ec9c6f25b05e0a6d532f
kwame@informatux.net <kwame@r2d2.informatux.local>
2023-03-06 21:49:55 -0600

    Initial commit, creating vpc and security groups

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+#output "security_group_id" {
+#  description = "The ID of the security group"
+#  value       = aws_security_group.id
+#}
+
+output "vpc_id" {
+  description = "fargate-vpc"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_arn" {
+  description = "fargate-vpc"
+  value       = module.vpc.vpc_arn
+}
+
+output "vpc_cidr_block" {
+  description = "fargate-vpc"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = module.vpc.default_security_group_id
+}
